a68241dc9c9e5eb58198540e4d95df9579b04dca
TangXudong <851988701@qq.com>
2017-09-13 23:36:47 +0800

    企业添加项目

--- app/src/main/java/com/example/oct/forestbank/ProjectAdd.java
+++ app/src/main/java/com/example/oct/forestbank/ProjectAdd.java
@@ -1,16 +1,31 @@
 package com.example.oct.forestbank;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class ProjectAdd extends AppCompatActivity {
+public class ProjectAdd extends AppCompatActivity
+        implements View.OnClickListener{
+
+    final int DATE_DIALOG=1;
+    int year1,month1,day1;
+    private Button setRaiseStartButton;
+
+    private TextView raiseStartTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_add);
+        initViews();
+        initEvents();
     }
 
     public static void actionStart(Context context){
@@ -18,4 +33,51 @@ public class ProjectAdd extends AppCompatActivity {
         context.startActivity(intent);
     }
 
+    private void initViews(){
+        setRaiseStartButton=(Button)
+                findViewById(R.id.set_raise_start_button);
+        raiseStartTime=(TextView)
+                findViewById(R.id.raise_start_time);
+    }
+
+    private void initEvents(){
+        setRaiseStartButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.set_raise_start_button:
+                showDialog(DATE_DIALOG);
+        }
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id){
+            case DATE_DIALOG:
+                return new DatePickerDialog(this, mdateListener, year1,month1,day1);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener mdateListener =
+            new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            year1 = year;
+            month1 = monthOfYear;
+            day1 = dayOfMonth;
+            display1();
+        }
+    };
+    /**
+     * 设置日期 利用StringBuffer追加
+     */
+    public void display1() {
+        raiseStartTime.setText(new StringBuffer().append(month1 + 1)
+                .append("-").append(day1).append("-").append(year1).append(" "));
+    }
 }
